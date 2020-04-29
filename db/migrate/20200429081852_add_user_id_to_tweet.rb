740d10c187f40c79120e6ce34d422827aecb3206
vagrant <vagrant@localhost.localdomain>
2020-04-29 17:22:36 +0900

    ログインしたユーザーのみつぶやけるようカラムを追加

---
+++ db/migrate/20200429081852_add_user_id_to_tweet.rb
@@ -0,0 +1,5 @@
+class AddUserIdToTweet < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :tweets, :user, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_150525) do
+ActiveRecord::Schema.define(version: 2020_04_29_081852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_04_28_150525) do
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema.define(version: 2020_04_28_150525) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
