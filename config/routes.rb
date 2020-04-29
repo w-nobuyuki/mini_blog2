93284f9738fa81b5d3196a9553262b375a20119f
vagrant <vagrant@localhost.localdomain>
2020-04-29 22:42:51 +0900

    deviseのルーティングを単数形に変更（自身への処理しか存在しないため）

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # HACK: registrationsコントローラのcancelとdestroyは利用していない
-  devise_for :users, only: [:sessions, :registrations]
+  devise_for :user, only: [:sessions, :registrations]
   resources :tweets, except: %i[edit show update index]
   resources :users, only: :show
   root 'tweets#index'
