20d62f77a2bf3e1e1330d2521f47abed515abc88
vagrant <vagrant@localhost.localdomain>
2020-05-03 23:35:25 +0900

    配列の書き方を修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # HACK: registrationsコントローラのcancelとdestroyは利用していない
-  devise_for :user, only: [:sessions, :registrations]
+  devise_for :user, only: %i[sessions registrations]
   resources :tweets, except: %i[edit show update index]
   resources :users, only: :show do
     member do
