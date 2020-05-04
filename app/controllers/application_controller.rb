15b943b0fb8c1b0fae4e7415c2cf6d6781d5c752
vagrant <vagrant@localhost.localdomain>
2020-05-04 15:21:46 +0900

    productionモードでのみbasic認証が適用されるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -9,5 +10,17 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
+  end
+
+  private
+
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials.basic[:username] && password == Rails.application.credentials.basic[:password]
+    end
   end
 end
