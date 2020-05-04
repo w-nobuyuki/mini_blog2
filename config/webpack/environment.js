6ec2ea92e7709172002b375397409e3f9db2363e
vagrant <vagrant@localhost.localdomain>
2020-05-04 16:35:22 +0900

    bootstrap4が適用されない問題は解決されなかったため一旦元に戻す

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack')
-
-environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    jQuery: 'jquery/dist/jquery',
-    Popper: 'popper.js/dist/popper'
-  })
-)
-
 module.exports = environment
