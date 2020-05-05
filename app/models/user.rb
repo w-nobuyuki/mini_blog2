9c106e0c6822be5bd1aa76b15a4e4e919ccf8fb3
vagrant <vagrant@localhost.localdomain>
2020-05-05 11:32:28 +0900

    ユーザーが削除されたときにツイートとフォローも削除されるよう関連を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable, authentication_keys: [:name]
 
-  has_many :tweets
-  has_many :follows, class_name: 'Follow', foreign_key: 'follower_id'
+  has_many :tweets, dependent: :destroy
+  has_many :follows, class_name: 'Follow', foreign_key: 'follower_id', dependent: :destroy
+  has_many :followed, class_name: 'Follow', foreign_key: 'follow_user_id', dependent: :destroy
   has_many :follow_users, through: :follows, source: :follow_user
   has_many :follow_users_tweets, through: :follow_users, source: :tweets
   has_many :likes, dependent: :destroy
